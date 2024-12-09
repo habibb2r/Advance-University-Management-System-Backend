4868d5ba9e7d9ddc43d9137df239990ab140d8f2
habibb2r <habibhk127@gmail.com>
2024-12-09 23:26:50 +0600

    Completed Academic Semster Interface

---
+++ src/Modules/AcademicSemester/academicSemester.interface.ts
@@ -0,0 +1,22 @@
+type Month =
+  | 'January'
+  | 'February'
+  | 'March'
+  | 'April'
+  | 'May'
+  | 'June'
+  | 'July'
+  | 'August'
+  | 'September'
+  | 'October'
+  | 'November'
+  | 'December';
+
+export type AcademicSemester ={
+    name: 'Autumn' | 'Fall' | 'Summer',
+    code: '01' | '02' | '03',
+    year: Date,
+    startMonth: Month,
+    endMonth: Month
+
+}
