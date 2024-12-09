7531a1758e96a6a68b8aef4fcf94abddf7c33669
habibb2r <habibhk127@gmail.com>
2024-12-09 19:33:38 +0600

    adding middleware to check data validation

--- src/Modules/User/user.route.ts
+++ src/Modules/User/user.route.ts
@@ -1,8 +1,17 @@
-import express from 'express';
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { NextFunction, Request, Response } from 'express';
 import { UserController } from './user.controller';
 
 const router = express.Router();
 
-router.post('/create-student', UserController.createStudent);
+const validationMiddleware = () =>{
+    return async(req: Request, res: Response, next: NextFunction)=>{
+        console.log('Checking by Army')
+        // next();
+    }
+}
+
+router.post('/create-student', validationMiddleware(), UserController.createStudent);
 
 export const UserRoutes = router;
