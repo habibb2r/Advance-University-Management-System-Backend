a93516e605baec94f46ccbee73872e3f515c82c5
habibb2r <habibhk127@gmail.com>
2024-12-10 23:35:38 +0600

    Created Academic Semester Controller

--- src/Modules/AcademicSemester/academicSemester.route.ts
+++ src/Modules/AcademicSemester/academicSemester.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { AcademicSemesterController } from './academicSemester.controller';
 
 
 const router = Router()
 
-router.post('/create-academic-semester', )
+router.post('/create-academic-semester', AcademicSemesterController.createAcademicSemester )
 
 export const AcademicRouter = router
