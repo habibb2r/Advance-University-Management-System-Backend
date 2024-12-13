f36a65e4fec7eeb7642871e967a0e56f683c26fe
habibb2r <habibhk127@gmail.com>
2024-12-14 01:10:29 +0600

    Added New Error Through with status code

--- src/Modules/AcademicDepartment/academicDepartment.model.ts
+++ src/Modules/AcademicDepartment/academicDepartment.model.ts
@@ -19,12 +19,24 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
+class AppError extends Error {
+    public statusCode: number;
+    constructor(statusCode: number, message: string, stack = ''){
+        super(message);
+        this.statusCode = statusCode;
+        if(stack){
+            this.stack = stack;
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
 academicDepartmentSchema.pre('save', async function(next){
     const isDepartmentExist = await AcademicDepartment.findOne({
         name: this.name,
     })
     if(isDepartmentExist) {
-        throw new Error('Department already exist');
+        throw new AppError(404,'Department already exist');
     }
     next()
 })
@@ -33,7 +45,7 @@ academicDepartmentSchema.pre('findOneAndUpdate', async function(next){
     const query = this.getQuery();
     const isDepartmentExist = await AcademicDepartment.findOne(query)
     if(!isDepartmentExist) {
-        throw new Error('Department does not exist');
+        throw new AppError(404,'Department does not exist');
     }
     next()
 })
