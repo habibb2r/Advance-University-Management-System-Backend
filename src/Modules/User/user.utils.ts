28cb5eb6314e48612239d0e037745b301fa9c3e3
habibb2r <habibhk127@gmail.com>
2024-12-13 22:37:14 +0600

    Initialized Academic Faculty

--- src/Modules/User/user.utils.ts
+++ src/Modules/User/user.utils.ts
@@ -4,12 +4,20 @@ import { User } from "./user.model";
 
 const findLastStudentId= async()=>{
     const lastStudent = await User.findOne({role: 'student'}, {id: 1, _id: 0}).sort({createdAt: -1}).lean();
-    return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+    return lastStudent?.id ? lastStudent.id : undefined;
 }
 
 export const generateStudentId = async(payload: TAcademicSemester)=>{
 
-    const currentId = await findLastStudentId() || (0).toString();
+    let currentId = (0).toString();
+    const lastStudentId = await findLastStudentId();
+    const lastStudentYear = lastStudentId?.substring(0, 4)
+    const lastStudentSemesterCode = lastStudentId?.substring(4, 6)
+    const currentSemesterCode = payload.code
+    const currentYear = payload.year
+    if(lastStudentId && lastStudentYear === currentYear && lastStudentSemesterCode === currentSemesterCode){
+        currentId = lastStudentId.substring(6)
+    }
     let incrementId = (Number(currentId)+ 1).toString().padStart(4, '0');
     incrementId = `${payload.year}${payload.code}${incrementId}`;
     return incrementId
