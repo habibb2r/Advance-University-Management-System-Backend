5c2efde6067c7ded94952c139a0b1325e28ee18a
habibb2r <habibhk127@gmail.com>
2024-12-16 00:54:09 +0600

    handled Zod simplifieer

--- src/Middlewares/globalErrorHandler.ts
+++ src/Middlewares/globalErrorHandler.ts
@@ -1,26 +1,50 @@
-import { path } from 'path';
+
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../Global-Interface/TError';
+import config from '../config';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
 
   //setting default values
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong';
-  type TErrorSource ={
-    path: string | number,
-    message: string
-  }[];
-    const errorSources : TErrorSource = [{
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong';
+
+    let errorSources : TErrorSource = [{
       path: "",
       message: 'Something went wrong'
   }]
+
+  const handleZodError =(err: ZodError)=>{
+    const errorSources : TErrorSource = err.issues.map((issue: ZodIssue)=>{
+      return {
+        path: issue?.path[issue.path.length - 1],
+        message: issue.message,
+      }
+    })
+
+    const statusCode = 400;
+    return {
+      statusCode,
+      message : 'Validation Error',
+      errorSources
+    }
+  }
+
+  if(err instanceof ZodError){
+   const simplifiedError = handleZodError(err)
+   statusCode = simplifiedError?.statusCode;
+   message = simplifiedError?.message;
+   errorSources = simplifiedError?.errorSources
+  }
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? err.stack : null,
   });
 };
 
--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ import path from 'path';
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
+  NODE_ENV: process.env.NODE_ENV,
   port: process.env.PORT,
   db: process.env.DB_URL,
   bcrypt_salt: process.env.BCRYPT_SALT,
