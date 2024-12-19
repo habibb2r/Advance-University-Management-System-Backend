913218dc115928c2085439a3315b40eb98c49b17
habibb2r <habibhk127@gmail.com>
2024-12-19 23:47:13 +0600

    Declaring methods

--- src/Builder/QueryBuilder.ts
+++ src/Builder/QueryBuilder.ts
@@ -1,6 +1,25 @@
-import { Query } from "mongoose";
+import { FilterQuery, Query } from "mongoose";
 
 class QueryBuilder<T>{
     public modelQuery : Query<T[], T>
-    public query : Record<string, unknown>
+    public query : Record<string, unknown>;
+
+    constructor(modelQuery : Query<T[], T>, query : Record<string, unknown>){
+        this.modelQuery = modelQuery;
+        this.query = query;
+    }
+
+    search(searchableFields: string[]){
+        if(this?.query?.searchTerm){
+            this.modelQuery = this.modelQuery.find({
+               $or: searchableFields.map((field)=>({
+                 [field]: { $regex: searchTerm, $options: 'i' }
+               }) as FilterQuery<T>)
+             })
+       }
+       }
+    }
+
+   
+
 }
